393b2159095487285b2618e05632101f4f80e210
Kristina Charkviani <kcharkviani@bog.ge>
2022-08-25 22:49:46 +0400

    slight temporary change

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function getTopicBtns(arr) {
   return arr
     .map((btn) => {
       return `<input type="button"  class="temp-btn btn" value="${btn}" id="${btn}">
-           `;
+             `;
     })
     .join(" ");
 }
@@ -34,12 +34,13 @@ function clickTopicButtons(arr) {
   arr.map((item) => {
     document.getElementById(`${item}`).addEventListener("click", () => {
        let submitApi = new Config(`search?q=${item}&`);
-
+      console.log('topics clicked')
       fetch(submitApi.getBaseUrl())
         .then((resp) => resp.json())
         .then((res) => {
           let gifs = new Gifs(res.data);
           gifs.render();
+          //style
           gfs.removeButtonClass(".btn", config.btnClassArr);
           document.getElementById(`${item}`).classList.add("btn-topic-active");
         });
@@ -56,6 +57,7 @@ searchField.addEventListener("click", () => {
 
 // trending button logic
 btnTrending.addEventListener("click", () => {
+  //style
   gfs.removeButtonClass(".btn", config.btnClassArr);
   btnTrending.classList.add("btn-trend-active");
 
@@ -70,23 +72,24 @@ btnTrending.addEventListener("click", () => {
 
 // submit button logic
 btnSubmit.addEventListener("click", () => {
+  //style
   gfs.removeButtonClass(".btn", config.btnClassArr);
   btnSubmit.classList.add("btn-subm-active");
-  let submitApi = new Config(`search?q=${searchField.value}&`);
 
+  let submitApi = new Config(`search?q=${searchField.value}&`);
   // temp
   if (searchField.value !== "") {
     if (config.topics.length > 5) {
-      // console.log("before shift", config.topics);
       config.topics.shift();
       config.topics.push(searchField.value);
-      // console.log("after shift & push", config.topics);
       let newTopics = config.topics;
       test.setContent("buttons", getTopicBtns(newTopics))
+      clickTopicButtons(config.topics)
     } else {
       config.topics.push(searchField.value);
       let newTopics = config.topics;
       test.setContent("buttons", getTopicBtns(newTopics))
+      clickTopicButtons(config.topics)
     }
   }
 
